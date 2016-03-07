f05a2b230e0a263299f65cef59665ea17d24a2af
Chris Nuernberger <chris@thinktopic.com>
2016-03-07 13:52:12 +0000

    Relying on official release for official release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
-                 [net.mikera/core.matrix "0.50.0-SNAPSHOT"]
+                 [net.mikera/core.matrix "0.50.0"]
                  [thi.ng/typedarrays "0.1.2"]]
 
   :plugins      [[lein-cljsbuild "1.0.6"]
